7c0534b6bd9c35638b48fab06cad5a1b794ba76d
asdlx97 <delalieux.arno@gmail.com>
2025-10-10 00:18:10 +0200

    Add introductory Python exercises for Part 1
    Added several beginner Python exercise scripts covering basic print statements, string manipulation, and user input. These exercises are intended for foundational programming practice. Also added a .DS_Store file, which may be unnecessary for version control.

---
+++ Exercises/Part 1/01-06_name_twice.py
@@ -0,0 +1,17 @@
+#Information From The User
+
+name = input("What's your name? ") #Input refers to any information a user gives to the program. Specifically, the Python command input reads in a line of input typed in by the user. It may also be used to display a message to the user, to prompt for specific input.
+print("Hi there, " + name + "!")
+
+"""Please write a program which asks for the user's name and then prints it twice, on two consecutive lines.
+
+An example of the how the program should function:
+
+Sample output
+What is your name? Paul
+Paul
+Paul"""
+
+name = input("What is your name? ")
+print(name)
+print(name)
