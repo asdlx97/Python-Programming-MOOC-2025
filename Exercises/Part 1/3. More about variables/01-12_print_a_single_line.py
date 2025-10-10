f711d3c9da4fd78a90fe092296d1954bcd8dc5fa
asdlx97 <delalieux.arno@gmail.com>
2025-10-10 22:10:38 +0200

    Add exercise to print calculation on a single line
    Introduces a Python exercise demonstrating how to use the 'end' argument in print statements to output a calculation on a single line without changing the number of print commands.

---
+++ Exercises/Part 1/3. More about variables/01-12_print_a_single_line.py
@@ -0,0 +1,37 @@
+"""
+EXERCISE
+Each print command usually prints out a line of its own, complete with a change of line at the end. 
+However, if the print command is given an additional argument end = "", it will not print a line change.
+
+For example:
+
+print("Hi ", end="")
+print("there!")
+Sample output
+Hi there!
+Please fix this program so that the entire calculation, complete with result, is printed out on a single line. 
+Do not change the number of print commands used.
+
+#Fix the code
+print(5)
+print(" + ")
+print(8)
+print(" - ")
+print(4)
+print(" = ")
+print(5 + 8 - 4)
+"""
+
+print(5, end="")
+print(" + ", end="")
+print(8, end="")
+print(" - ", end="")
+print(4, end="")
+print(" = ", end="")
+print(5 + 8 - 4, end="")
+
+#Review
+"""
+Great to know this technique, especially if you have to start a new print statement to present 
+results from another calculation or a function but want it on the same line as the previous print? 
+"""
