846b6c92a083b3ef17c75c42df485f02ad9b1d29
asdlx97 <delalieux.arno@gmail.com>
2025-10-12 21:18:01 +0200

    Solve exercise to compute sum and product of two numbers
    Solved exercise that prompts the user for two numbers and prints their sum and product. Includes both my solution and a suggested solution for comparison.

---
+++ Exercises/Part 1/4. Arithmetic operations/01-17_sum_and_product.py
@@ -0,0 +1,32 @@
+"""
+Please write a program which asks the user for two numbers. The program will then print out the sum and the product of the two numbers.
+
+The program should function as follows:
+
+Sample output
+Number 1: 3
+Number 2: 7
+The sum of the numbers: 10
+The product of the numbers: 21
+
+"""
+# Write your solution here
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+
+print(f"The sum of the numbers: {number1 + number2}")
+print(f"The product of the numbers: {number1 * number2}")
+
+"""
+#Suggested Solution
+
+number1 = int(input('Number 1: '))
+number2 = int(input('Number 2: '))
+print("The sum of the numbers:", number1 + number2)
+print("The product of the numbers:", number1 * number2)
+ 
+ 
+#Review
+My solution arrives at the same output. The only difference is that I used an f-string fot the print.
+
+"""
