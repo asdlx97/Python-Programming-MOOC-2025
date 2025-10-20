5aa67888c2064bc5bb8273433c85048ea2c04f25
asdlx97 <delalieux.arno@gmail.com>
2025-10-20 21:32:10 +0200

    Add solved story-building loop exercise with end conditions
    Implements a program that collects words from user input to form a story, ending when the user types 'end' or repeats a word consecutively. Includes both the initial and improved versions as well as sample outputs and a suggested solution.

---
+++ Exercises/Part 2/4. Simple loops/02-21_story.py
@@ -0,0 +1,84 @@
+"""
+#Part 1
+
+Please write a program which keeps asking the user for words. If the user types in end, 
+the program should print out the story the words formed, and finish.
+
+Sample output
+Please type in a word: Once
+Please type in a word: upon
+Please type in a word: a
+Please type in a word: time
+Please type in a word: there
+Please type in a word: was
+Please type in a word: a
+Please type in a word: girl
+Please type in a word: end
+Once upon a time there was a girl
+
+#Part 2
+
+Change the program so that the loop ends also if the user types in the same word twice in a row.
+
+Sample output
+Please type in a word: It
+Please type in a word: was
+Please type in a word: a
+Please type in a word: dark
+Please type in a word: and
+Please type in a word: stormy
+Please type in a word: night
+Please type in a word: night
+It was a dark and stormy night
+
+"""
+#Part 1
+# Write your solution here
+wording = ""
+
+while True:
+
+    word = input("Please type in a word: ")
+
+    if word == "end":
+        break
+
+    wording += word + " "
+
+print(wording)
+
+#Part 2
+# Write your solution here
+wording = ""
+previous_word = ""
+
+while True:
+
+    word = input("Please type in a word: ")
+
+    if word == "end" or word == previous_word:
+        break
+
+    wording += word + " "
+    previous_word = word
+
+print(wording)
+
+"""
+# Suggested solution
+
+story = ""
+previous = ""
+while True:
+    word = input("Please type in a word: ")
+    if word == "end" or word == previous:
+        break
+    story += word + " "
+    previous = word
+ 
+print(story)
+ 
+
+#Review
+Same output, same script. This was fun to think about!!
+"""
