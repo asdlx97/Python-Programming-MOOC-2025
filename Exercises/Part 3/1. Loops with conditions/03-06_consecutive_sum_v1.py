f8e22eda774168384269c6331c047397146c2b5f
asdlx97 <delalieux.arno@gmail.com>
2025-10-23 22:50:49 +0200

    Add solved exercise to sum consecutive numbers to a limit
    Implements a script that prompts the user for a limit and calculates the sum of consecutive numbers starting from 1 until the sum is at least the given limit. Includes both the main solution and a commented suggested solution for comparison.

---
+++ Exercises/Part 3/1. Loops with conditions/03-06_consecutive_sum_v1.py
@@ -0,0 +1,49 @@
+"""
+Please write a program which asks the user to type in a limit. 
+The program then calculates the sum of consecutive numbers (1 + 2 + 3 + ...) 
+until the sum is at least equal to the limit set by the user. 
+
+The program should function as follows:
+
+Sample output
+Limit: 2
+3
+
+Sample output
+Limit: 10
+10
+
+Sample output
+Limit: 18
+21
+If you have trouble understanding how the desired output is calculated, 
+the sample outputs in the next exercise may help. 
+You may assume the number typed in by the user is always equal to 2 or higher.
+
+"""
+# Write your solution here
+limit = int(input("Limit: "))
+counter = 1
+sum = 0
+
+while sum < limit:
+    sum += counter
+    counter += 1
+
+print(sum)
+
+"""
+# Suggested solution
+
+limit = int(input("Limit: "))
+number = 1
+sum = 1
+while sum < limit:
+    number += 1
+    sum += number
+print(sum)
+
+#Review
+My solution results in the same output, suggested one manually starts the sum at 1, which I think
+is a bit less intuitive as I rather let the loop looping.
+"""  
