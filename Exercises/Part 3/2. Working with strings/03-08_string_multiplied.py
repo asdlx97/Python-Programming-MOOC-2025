fe041383868afca7a1e278bae30f58da4f470577
asdlx97 <delalieux.arno@gmail.com>
2025-10-26 21:29:36 +0100

    Add solved string multiplication exercise script
    Added a new script that prompts the user for a string and an amount, then prints the string repeated the specified number of times on one line. This is part of the exercises for working with strings.

---
+++ Exercises/Part 3/2. Working with strings/03-08_string_multiplied.py
@@ -0,0 +1,30 @@
+"""
+Please write a program which asks the user for a string and an amount. 
+The program then prints out the string as many times as specified by the amount. 
+The printout should all be on one line, with no extra spaces or symbols added.
+
+An example of expected behaviour:
+
+Sample output
+Please type in a string: hiya
+Please type in an amount: 4
+hiyahiyahiyahiya
+
+"""
+# Write your solution here
+words = input("Please type in a string: ")
+amount = int(input("Please type in an amount: "))
+
+print(words*amount)
+
+
+"""
+# Suggested solution
+
+input_string = input("Please type in a string: ")
+amount = int(input("Please type in an amount: "))
+print (input_string * amount)
+
+#Review
+Same output, same script.
+"""  
