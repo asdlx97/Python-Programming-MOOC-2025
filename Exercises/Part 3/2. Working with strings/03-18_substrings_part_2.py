9612bbcebf77a0250859ce959f60db73d9115d79
asdlx97 <delalieux.arno@gmail.com>
2025-10-28 22:11:21 +0100

    Add solved exercise to print substrings ending with last char
    Implements a script that prompts the user for a string and prints all substrings ending with the last character, from shortest to longest. Includes both the user's solution and a suggested solution for comparison.

---
+++ Exercises/Part 3/2. Working with strings/03-18_substrings_part_2.py
@@ -0,0 +1,36 @@
+"""
+Please write a program which asks the user to type in a string. 
+The program then prints out all the substrings which end with the last character, 
+from the shortest to the longest. Have a look at the example below.
+
+Sample output
+Please type in a string: test
+t
+st
+est
+test
+
+"""
+# Write your solution here
+input_string = input("Please type in a string: ")
+loop_variable = len(input_string)-1 #Minus one otherwise it excludes at first print, results in empty line
+
+while loop_variable >= 0: #We count down so use 0 as a limit instead of len(input_string)
+    print(input_string[loop_variable:]) #Suffices to put the loop variable at the start
+    loop_variable -= 1 #Counting down instead of up
+
+"""
+# Suggested solution
+
+string = input("Please type in a string: ")
+ 
+start = len(string) - 1
+while start >= 0:
+    print(string[start:])
+    start -= 1
+
+#Review
+Same script, same output. Was more work than expected to adapt from previous exercise, 
+but lots of fun!
+
+""" 
