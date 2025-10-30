6eab8e1339f6a234d951c00849b3117664ae0f0b
asdlx97 <delalieux.arno@gmail.com>
2025-10-30 22:16:02 +0100

    Add solved exercise to print first letters of words
    Added a script that prompts the user for a sentence and prints the first letter of each word on a separate line. Includes both the author's and a suggested solution for extracting first letters.

---
+++ Exercises/Part 3/3. More loops/03-24_first_letters_of_words.py
@@ -0,0 +1,48 @@
+"""
+Please write a program which asks the user to type in a sentence. 
+The program then prints out the first letter of each word in the sentence, 
+each letter on a separate line.
+
+An example of expected behaviour:
+
+Sample output
+Please type in a sentence: Humpty Dumpty sat on a wall
+H
+D
+s
+o
+a
+w
+
+"""
+# Write your solution here
+sentence = input("Please type in a sentence: ")
+i = 1
+
+print(sentence[0]) #We can already print first letter (of first word)
+
+while i <= len(sentence):
+    if sentence[i-1] == " ":
+        print(sentence[i])
+
+    i += 1
+
+"""
+# Suggested solution
+
+sentence = input("Please type in a sentence: ")
+ 
+# Add a space at the start, to make handling sentence easier
+sentence = " " + sentence
+ 
+# Searching for indexes which are preceded by spaces
+index = 1
+while index < len(sentence):
+    if sentence[index-1] == " " and sentence[index] != " ":
+        print(sentence[index])
+    index += 1
+
+#Review
+My solution results in the same output.
+
+""" 
