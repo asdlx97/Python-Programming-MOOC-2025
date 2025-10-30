cf8c35a138f151d19f61ea02eb5013bd2d4113ae
asdlx97 <delalieux.arno@gmail.com>
2025-10-30 22:37:17 +0100

    Add solved factorial calculator exercise
    Implements a loop-based program that prompts the user for an integer and prints its factorial, terminating when a non-positive number is entered. Includes both the main solution and a suggested alternative approach.

---
+++ Exercises/Part 3/3. More loops/03-25_factorial.py
@@ -0,0 +1,67 @@
+"""
+Please write a program which asks the user to type in an integer number. 
+If the user types in a number equal to or below 0, the execution ends. 
+Otherwise the program prints out the factorial of the number.
+
+The factorial of a number involves multiplying the number by all the positive 
+integers smaller than itself. In other words, it is the product of all positive 
+integers less than or equal to the number. For example, the factorial of 5 is 1 * 2 * 3 * 4 * 5 = 120.
+
+Some examples of expected behaviour:
+
+Sample output
+Please type in a number: 3
+The factorial of the number 3 is 6
+Please type in a number: 4
+The factorial of the number 4 is 24
+Please type in a number: -1
+Thanks and bye!
+
+Sample output
+Please type in a number: 1
+The factorial of the number 1 is 1
+Please type in a number: 0
+Thanks and bye!
+
+"""
+# Write your solution here
+while True:
+    number = int(input("Please type in a number: "))
+    if number <= 0:
+        break
+    
+    factorial = number
+    i = number - 1
+
+    while i > 0:
+        factorial *= i
+        i -= 1
+        
+    print(f"The factorial of the number {number} is {factorial}")
+    
+print(f"Thanks and bye!")
+
+"""
+# Suggested solution
+
+while True:
+    number = int(input("Please type in a number: "))
+    if number <= 0:
+        break
+ 
+    factorial = 1
+    new = 1
+    while new <= number:
+        factorial *= new
+        new += 1
+ 
+    print(f"The factorial of the number {number} is {factorial}")
+ 
+print("Thanks and bye!")
+
+#Review
+Same output, almost same script. Suggested one differs regarding counting direction where it
+ascends instead of descends like in ly solution. It multiplies upwards, but this doesn't make a difference.
+
+#TODO Maybe try to do this again using build in factorials in standard math library
+""" 
