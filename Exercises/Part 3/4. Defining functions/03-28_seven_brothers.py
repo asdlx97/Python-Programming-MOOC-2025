dc50917d210131fca42959a78b4581ff41cd7fcf
asdlx97 <delalieux.arno@gmail.com>
2025-10-31 20:21:30 +0100

    Add solved seven_brothers function implementation exercise
    Implements the seven_brothers function which prints the names of the seven brothers in alphabetical order. Includes a main block for testing the function.

---
+++ Exercises/Part 3/4. Defining functions/03-28_seven_brothers.py
@@ -0,0 +1,51 @@
+"""
+Please write a function named seven_brothers. When the function is called, 
+it should print out the names of the seven brothers in alphabetical order, 
+as in the example below. See the similarly named exercise in part 1 for more details about the brothers.
+
+Sample output
+Aapo
+Eero
+Juhani
+Lauri
+Simeoni
+Timo
+Tuomas
+1234
+
+"""
+# Write your solution here
+def seven_brothers():
+    print("Aapo")
+    print("Eero")
+    print("Juhani")
+    print("Lauri")
+    print("Simeoni")
+    print("Timo")
+    print("Tuomas")
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+  seven_brothers()
+
+"""
+# Suggested solution
+
+def seven_brothers():
+    print("Aapo")
+    print("Eero")
+    print("Juhani")
+    print("Lauri")
+    print("Simeoni")
+    print("Timo")
+    print("Tuomas")
+    
+if __name__ == "__main__":
+    seven_brothers()
+
+    
+#Review
+Same output, slightly different script. Suggested solution does the “middle number check”
+outside of the loop.
+
+""" 
