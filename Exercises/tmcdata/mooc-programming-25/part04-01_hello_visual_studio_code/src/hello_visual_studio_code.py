e3a170f5f632676e4cc24bb65a968d9c0da8b9ab
asdlx97 <delalieux.arno@gmail.com>
2025-11-03 23:26:34 +0100

    Add part04 exercises and TMC support files
    Added all exercises for part04 of mooc-programming-25, including source code, tests, and TMC configuration files for each exercise. This initial commit sets up the structure and content for the course exercises.

---
+++ Exercises/tmcdata/mooc-programming-25/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -0,0 +1,71 @@
+"""
+Please write a program which asks the user which editor they are using. 
+The program should keep on asking until the user types in Visual Studio Code.
+Have a look at the example of expected behaviour below:
+
+Sample output
+Editor: Emacs
+not good
+Editor: Vim
+not good
+Editor: Word
+awful
+Editor: Atom
+not good
+Editor: Visual Studio Code
+an excellent choice!
+
+If the user types in Word or Notepad, the program counters with awful. 
+Other unacceptable editor choices receive the reply not good.
+
+The program should be case-insensitive in its reactions. 
+That is, the same user input in lowercase, uppercase or mixed case should trigger the same reaction:
+
+Sample output
+Editor: NOTEPAD
+awful
+Editor: viSUal STudiO cODe
+an excellent choice!
+
+Hint: The simplest way to achieve this is converting all characters to the same case. 
+The Python string method lower converts a string to lowercase entirely. 
+
+An example of its use:
+
+mystring = "Visual Studio CODE"
+if "visual studio code" == mystring.lower():
+    print("this was the string I was looking for!")
+
+NB: this exercise doesn't ask you to write any functions, 
+so you should not place any code within an if __name__ == "__main__" block. 
+The same applies to any upcoming exercises that don't explicitly ask for functions.
+"""
+
+# Write your solution here
+while True:
+    editor = input("Editor: ").lower()
+    if editor == "visual studio code":
+        print("an excellent choice!")
+        break
+    elif editor == "word" or editor == "notepad":
+        print("awful")
+    else:
+        print("not good")
+
+
+
+"""
+# Suggested Solution
+while True:
+    editor = input("Editor: ").lower()
+    if editor == "visual studio code":
+        break
+    if editor == "word" or editor == "notepad":
+        print("awful")
+    else:
+        print("not good")
+print("an excellent choice!")
+
+#Review
+Same script, same output.
+"""
---
+++ Exercises/tmcdata/mooc-programming-25/part04-02_line/src/line.py
@@ -0,0 +1,6 @@
+# Write your solution here
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    line(5, "x")
+
+    
