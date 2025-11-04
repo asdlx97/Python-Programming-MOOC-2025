9fa3417276b25ee1c5f9ba48d0fb9b566a5c49e3
asdlx97 <delalieux.arno@gmail.com>
2025-11-04 23:08:04 +0100

    Add solved exercise implementing line function with customizable character
    Added the line function that prints a line of specified length using the first character of the provided string, or '*' if the string is empty. Included sample usage and a commented review comparing two solution approaches.

--- Exercises/tmcdata/mooc-programming-25/part04-02_line/src/line.py
+++ Exercises/tmcdata/mooc-programming-25/part04-02_line/src/line.py
@@ -1,6 +1,43 @@
+"""
+Please write a function named line, which takes two arguments: 
+an integer and a string. The function prints out a line of text, 
+the length of which is specified by the first argument. 
+
+The character used to draw the line should be the first character in the second argument. 
+If the second argument is an empty string, the line should consist of stars.
+
+An example of expected behaviour:
+
+line(7, "%")
+line(10, "LOL")
+line(3, "")
+
+Sample output
+%%%%%%%
+LLLLLLLLLL
+***
+"""
+
 # Write your solution here
+def line (length, string):
+    if string == "":
+        print("*" * length)
+    else:
+        print(string[0] * length)
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     line(5, "x")
 
-    
+"""
+# Suggested Solution
+
+def line(size, character):
+    if character == "":
+        character = "*"
+    print(character[0] * size)
+
+#Review
+My solution results in the same output, suggested one uses fewer lines
+as it reassigns new value to argument if it's an empty string.
+"""
