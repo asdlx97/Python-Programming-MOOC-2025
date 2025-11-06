9033cd2d4d7c1604aef43a32c2c02f72f3ce2c76
asdlx97 <delalieux.arno@gmail.com>
2025-11-06 09:01:30 +0100

    Add solved exercise implementing triangle function using line helper
    Added the triangle function that prints a triangle of hashes with height and width equal to the given argument. The function uses the provided line function to print each row, as required by the exercise.

--- Exercises/tmcdata/mooc-programming-25/part04-06_triangle/src/triangle.py
+++ Exercises/tmcdata/mooc-programming-25/part04-06_triangle/src/triangle.py
@@ -1,9 +1,65 @@
+"""
+Please write a function named triangle, which draws a triangle of hashes, and takes one argument. 
+The triangle should be as tall and as wide as the value of the argument.
+
+The function should call the function line from the exercise above for the actual printing out. 
+Copy your solution to that exercise above the code for this exercise. Please don't change anything in the line function.
+
+Some examples:
+
+triangle(6)
+print()
+triangle(3)
+
+Sample output
+#
+##
+###
+####
+#####
+######
+
+#
+##
+###
+"""
+
 # Copy here code of line function from previous exercise
+def line (length, string):
+    if string == "":
+        print("*" * length)
+    else:
+        print(string[0] * length)
 
 def triangle(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    triangle_length = 1
+    i = size
+
+    while i > 0:
+        line(triangle_length, "#")
+        i -= 1
+        triangle_length += 1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     triangle(5)
+
+"""
+# Suggested Solution
+
+def line(size, character):
+    if character == "":
+        character = "*"
+    print(character[0] * size)
+ 
+def triangle(size):
+    i = 0
+    while i < size:
+        i += 1
+        line(i, '#')
+
+#Review
+My suggestion results in the same output. Suggested one uses only one variable and the same variable for bith the loop variable 
+and the size argument for the line function, which makes totally sense and is more efficiënt. 
+"""
