327514a96c4282ae2a49263832f928ab38766b8a
asdlx97 <delalieux.arno@gmail.com>
2025-11-07 22:47:47 +0100

    Add solved exercise implementing word uniqueness checker
    Added a program that prompts the user for words and exits when a word is entered for the second time, displaying the count of unique words entered. Includes both the solution and a suggested alternative for comparison.

--- Exercises/tmcdata/mooc-programming-25/part04-15_same_word_twice/src/same_word_twice.py
+++ Exercises/tmcdata/mooc-programming-25/part04-15_same_word_twice/src/same_word_twice.py
@@ -1,1 +1,44 @@
+"""
+Please write a program which asks the user for words. 
+If the user types in a word for the second time, 
+the program should print out the number of different words typed in, and exit.
+
+Sample output
+Word: once
+Word: upon
+Word: a
+Word: time
+Word: upon
+You typed in 4 different words
+
+NB: this exercise doesn't ask you to write any functions, 
+so you should not place any code within an if __name__ == "__main__" block.
+"""
+
 # Write your solution here
+my_list = []
+
+while True:
+    answer = input("Word: ")
+    if answer in my_list:
+        break
+    else:
+        my_list.append(answer)
+
+print(f"You typed in {len(my_list)} different words")
+
+"""
+# Suggested Solution
+
+words = []
+while True:
+    word = input("Word: ")
+    if word in words:
+        break
+    words.append(word)
+ 
+print("You typed in", len(words), "different words")
+
+#Review
+My suggestion results in the same output. Suggested one doesn't use else statement as it is not needed indeed.
+"""
