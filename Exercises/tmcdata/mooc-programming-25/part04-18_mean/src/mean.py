ec05baf8152ad0766d61c6ac7b19380198eb469d
asdlx97 <delalieux.arno@gmail.com>
2025-11-07 22:56:29 +0100

    Add solved exercise regarding mean function to calculate arithmetic mean
    Implemented the mean function that takes a list of integers and returns their arithmetic mean. Added sample usage and explanatory comments to clarify the function's purpose and expected output.

--- Exercises/tmcdata/mooc-programming-25/part04-18_mean/src/mean.py
+++ Exercises/tmcdata/mooc-programming-25/part04-18_mean/src/mean.py
@@ -1,6 +1,30 @@
+"""
+Please write a function named mean, which takes a list of integers as an argument. 
+The function returns the arithmetic mean of the values in the list.
+
+my_list = [1, 2, 3, 4, 5]
+result = mean(my_list))
+print("mean value is", result)
+
+Sample output
+mean value is 3.0
+"""
+
 # Write your solution here
+def mean(a_list):
+    return sum(a_list) / len(a_list)
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
     result = mean(my_list)
     print(result)
+
+"""
+# Suggested Solution
+
+def mean(my_list : list):
+    return sum(my_list) / len(my_list)
+
+#Review
+Same output, same script
+"""
