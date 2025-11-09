c79e0dd3f5f3495a456405e5ef29618c0517a530
asdlx97 <delalieux.arno@gmail.com>
2025-11-09 22:29:29 +0100

    Add solved exercise implementing anagrams function
    Added the anagrams function to check if two strings are anagrams by comparing their sorted characters. Includes example usage and explanatory comments.

--- Exercises/tmcdata/mooc-programming-25/part04-23_anagrams/src/anagrams.py
+++ Exercises/tmcdata/mooc-programming-25/part04-23_anagrams/src/anagrams.py
@@ -1,1 +1,33 @@
+"""
+Please write a function named anagrams, 
+which takes two strings as arguments. 
+
+The function returns True if the strings are anagrams of each other. 
+Two words are anagrams if they contain exactly the same characters.
+
+Some examples of how the function should work:
+
+print(anagrams("tame", "meta")) # True
+print(anagrams("tame", "mate")) # True
+print(anagrams("tame", "team")) # True
+print(anagrams("tabby", "batty")) # False
+print(anagrams("python", "java")) # False
+Hint: the function sorted can be used on strings as well.
+"""
+
 # Write your solution here
+def anagrams(word1: str, word2: str):
+    return sorted(word1) == sorted(word2)
+
+if __name__ == "__main__":
+    print(anagrams("tame", "meta"))
+
+"""
+# Suggested Solution
+
+def anagrams(string1: str, string2: str):
+    return sorted(string1) == sorted(string2)
+
+#Review
+Same script, same output. 
+"""
