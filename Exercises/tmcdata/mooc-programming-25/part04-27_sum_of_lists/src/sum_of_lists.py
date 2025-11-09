57b8a1b787ca4cae8dfb8320790704ce2fc62305
asdlx97 <delalieux.arno@gmail.com>
2025-11-09 23:35:12 +0100

    Add solved exercise implementing list_sum function to sum elements of two lists
    Added the list_sum function which takes two lists of integers and returns a new list containing the sums of corresponding elements. Includes example usage and suggested alternative solutions in comments.

--- Exercises/tmcdata/mooc-programming-25/part04-27_sum_of_lists/src/sum_of_lists.py
+++ Exercises/tmcdata/mooc-programming-25/part04-27_sum_of_lists/src/sum_of_lists.py
@@ -1,1 +1,50 @@
+"""
+Please write a function named list_sum which takes 
+two lists of integers as arguments. 
+
+The function returns a new list which contains the sums of 
+the items at each index in the two original lists. 
+
+You may assume both lists have the same number of items.
+
+An example of the function at work:
+
+a = [1, 2, 3]
+b = [7, 8, 9]
+print(list_sum(a, b)) # [8, 10, 12]
+"""
+
 # Write your solution here
+def list_sum(list1: list, list2: list):
+    sum_list = []
+
+    for number in range(0, len(list1)):
+        sum = list1[number] + list2[number]
+
+        sum_list.append(sum)
+    
+    return sum_list
+
+if __name__ == "__main__":
+    a = [1, 2, 3]
+    b = [7, 8, 9]
+    print(list_sum(a, b))
+
+"""
+# Suggested Solution
+
+def list_sum(list1: list, list2: list):
+    results = []
+    for i in range(len(list1)):
+        results.append(list1[i] + list2[i])
+ 
+    return results
+# Another solution would be use zip-function,
+# which creates new list by combining items in two or more lists
+# for item1, item2 in zip(list1, list2):
+#   results.append(item1 + item2)
+# Write your solution here
+
+#Review
+Same output, same script!! 
+"""
