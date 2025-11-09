e8a195f671341175bb97fcbec845d0b8c45de149
asdlx97 <delalieux.arno@gmail.com>
2025-11-09 23:19:58 +0100

    Add solved exercise implementing sum_of_positives function
    Added the sum_of_positives function to calculate the sum of non-negative numbers in a list. Also included a commented suggested solution for reference.

--- Exercises/tmcdata/mooc-programming-25/part04-25_sum_of_positives/src/sum_of_positives.py
+++ Exercises/tmcdata/mooc-programming-25/part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,24 @@
 # Write your solution here
+def sum_of_positives(my_list: list):
+    sum = 0
+
+    for number in my_list:
+        if number >= 0:
+            sum += number
+
+    return sum
+
+
+"""
+# Suggested Solution
+
+def sum_of_positives(my_list: list):
+    sum = 0
+    for item in my_list:
+        if item > 0:
+            sum += item
+    return sum
+
+#Review
+Same output, same script!! 
+"""
