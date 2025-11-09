9f6369abcfb00de6061fb67a5659bfd6a9d8d4e1
asdlx97 <delalieux.arno@gmail.com>
2025-11-09 21:08:49 +0100

    Add solved exercise implementing functions to extract words from a string
    Added first_word, second_word, and last_word functions to extract the first, second, and last words from a given string. Updated the main block to test these functions with a sample sentence.

--- Exercises/tmcdata/mooc-programming-25/part04-11_first_second_last/src/first_second_last.py
+++ Exercises/tmcdata/mooc-programming-25/part04-11_first_second_last/src/first_second_last.py
@@ -1,7 +1,44 @@
+
+
 # Write your solution here
+def first_word(string):
+    length = len(string)
+    index = 0
+
+    while index < length:
+        if string[index] == " ":
+            return string[0:index]
+            break
+        index += 1
+
+def second_word(string):
+    length = len(string)
+    start_index = len(first_word(string)) + 1
+    index = start_index
+
+    while index < length:
+        if string[index] == " ":
+            return string[start_index:index]
+            break
+        index += 1
+    
+    return string[start_index:length]
+
+
+def last_word(string):
+    length = -len(string)
+    index = -1
+
+    while index > length:
+        if string[index] == " ":
+            return f"{string[index+1:-1]}{string[-1]}"
+            break
+        index -= 1
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    sentence = "once upon a time there was a programmer"
+    sentence = "sumin gaat"
     print(first_word(sentence))
     print(second_word(sentence))
     print(last_word(sentence))
+
