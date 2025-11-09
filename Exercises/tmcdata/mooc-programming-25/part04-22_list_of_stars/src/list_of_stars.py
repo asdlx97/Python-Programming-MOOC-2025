3bb1aaf864d7ac5760e236e0f94966c537072e36
asdlx97 <delalieux.arno@gmail.com>
2025-11-09 22:23:38 +0100

    Add solved exercise implementing list_of_stars function
    Added the list_of_stars function to print lines of stars based on a list of integers. Includes sample usage and explanatory comments.

--- Exercises/tmcdata/mooc-programming-25/part04-22_list_of_stars/src/list_of_stars.py
+++ Exercises/tmcdata/mooc-programming-25/part04-22_list_of_stars/src/list_of_stars.py
@@ -1,1 +1,35 @@
+"""
+Please write a function named list_of_stars, 
+which takes a list of integers as its argument. 
+
+The function should print out lines of star characters. The numbers in the list specify how many stars each line should contain.
+
+For example, with the function call list_of_stars([3, 7, 1, 1, 2]) 
+the following should be printed out:
+
+Sample output
+***
+*******
+*
+*
+**
+"""
+
 # Write your solution here
+def list_of_stars(stars):
+    for star in stars:
+        print(f"{'*' * star}")
+
+if __name__ == "__main__":
+    list_of_stars([3, 7, 1, 1, 2])
+
+"""
+# Suggested Solution
+
+def list_of_stars(my_list: list):
+    for number in my_list:
+        print("*" * number)
+
+#Review
+Same script, same output. 
+"""
