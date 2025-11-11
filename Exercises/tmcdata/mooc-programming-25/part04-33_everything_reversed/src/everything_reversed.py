aab25ef285d672967de53348f7f0e1f19685ba91
asdlx97 <delalieux.arno@gmail.com>
2025-11-11 15:39:22 +0100

    Add solved exercise implementing everything_reversed function
    Added the everything_reversed function to reverse both the order and contents of strings in a list. Includes example usage and a comparison with a suggested solution.

--- Exercises/tmcdata/mooc-programming-25/part04-33_everything_reversed/src/everything_reversed.py
+++ Exercises/tmcdata/mooc-programming-25/part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,40 @@
+"""Please write a function named everything_reversed, which takes a list of strings as its argument. The function returns a new list with all of the items on the original list reversed. Also the order of items should be reversed on the new list.
+
+An example of how the function should work:
+
+my_list = ["Hi", "there", "example", "one more"]
+new_list = everything_reversed(my_list)
+print(new_list)
+Sample output
+['erom eno', 'elpmaxe', 'ereht', 'iH']"""
+
+
 # Write your solution here
+def everything_reversed(my_list: list):
+    reversed_list = []
+
+    for word in my_list[::-1]:
+        reversed_list.append(word[::-1])
+
+    return reversed_list
+
+
+if __name__ == "__main__":
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
+
+"""
+# Suggested Solution
+
+def everything_reversed(my_list: list):
+    new_list = []
+    for my_string in my_list:
+        new_list.append(my_string[::-1])
+    return new_list[::-1]
+
+#Review
+My solutions results in the same output, the only difference
+is that the suggested one reverses the new list at the end,
+while I do that while looping through it.
+"""
