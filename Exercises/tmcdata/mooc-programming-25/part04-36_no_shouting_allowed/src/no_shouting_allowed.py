20f4668772dde57d9e4482d93a35bd90523664ae
asdlx97 <delalieux.arno@gmail.com>
2025-11-11 16:54:31 +0100

    Add solved exercise implementing no_shouting function to filter uppercase strings
    Added the no_shouting function which returns a list excluding strings that are all uppercase. Includes example usage and comparison with a suggested solution.

--- Exercises/tmcdata/mooc-programming-25/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ Exercises/tmcdata/mooc-programming-25/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,63 @@
+"""The Python string method isupper() returns True if a string consists of only uppercase characters.
+
+Some examples:
+
+print("XYZ".isupper())
+
+is_it_upper = "Abc".isupper()
+print(is_it_upper)
+Sample output
+True
+False
+Please use the isupper method to write a function named no_shouting, which takes a list of strings as an argument. The function returns a new list, containing only those items from the original which do not consist of solely uppercase characters.
+
+An example of expected behaviour:
+
+my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+pruned_list = no_shouting(my_list)
+print(pruned_list)
+Sample output
+['def', 'lower', 'another lower', 'Capitalized']"""
+
+
 # Write your solution here
+def no_shouting(my_list: list):
+    no_allcaps_list = []
+
+    for word in my_list:
+        if word.isupper() == False:
+            no_allcaps_list.append(word)
+
+    return no_allcaps_list
+
+
+if __name__ == "__main__":
+    my_list = [
+        "ABC",
+        "def",
+        "UPPER",
+        "ANOTHERUPPER",
+        "lower",
+        "another lower",
+        "Capitalized",
+    ]
+
+    pruned_list = no_shouting(my_list)
+    print(pruned_list)
+
+"""
+# Suggested Solution
+
+def no_shouting(my_list: list):
+    without_upper = []
+ 
+    for string in my_list:
+        if not string.isupper():
+            without_upper.append(string)
+ 
+    return without_upper
+
+#Review
+My solutions results in the same output, the suggested one uses the "if not" as a
+conditional statement which is the only difference.
+"""
