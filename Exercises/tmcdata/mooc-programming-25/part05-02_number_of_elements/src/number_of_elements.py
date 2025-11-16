4296d3636a60fd38b71664d3ec6987f4d546705e
asdlx97 <delalieux.arno@gmail.com>
2025-11-16 19:19:29 +0100

    Add solved exercise implementing count_matching_elements function
    Added the count_matching_elements function to count occurrences of a given integer in a 2D matrix. Includes example usage and a suggested solution for reference.

--- Exercises/tmcdata/mooc-programming-25/part05-02_number_of_elements/src/number_of_elements.py
+++ Exercises/tmcdata/mooc-programming-25/part05-02_number_of_elements/src/number_of_elements.py
@@ -1,1 +1,41 @@
+"""Please write a function named count_matching_elements(my_matrix: list, element: int), which takes a two-dimensional array of integers and a single integer value as its arguments. The function then counts how many elements within the matrix match the argument value.
+
+An example of how the function should work:
+
+m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+print(count_matching_elements(m, 1))
+
+Sample output
+3"""
+
+
 # Write your solution here
+def count_matching_elements(my_matrix: list, element: int):
+    n_matches = 0
+
+    for row in my_matrix:
+        for item in row:
+            if item == element:
+                n_matches += 1
+
+    return n_matches
+
+
+if __name__ == "__main__":
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    print(count_matching_elements(m, 1))
+
+"""
+# Suggested Solution
+
+def count_matching_elements(my_matrix: list, elements: int):
+    n = 0
+    for row in my_matrix:
+        for cell in row:
+            if cell == elements:
+                n += 1
+    return n
+
+#Review
+same output, same script.
+"""
