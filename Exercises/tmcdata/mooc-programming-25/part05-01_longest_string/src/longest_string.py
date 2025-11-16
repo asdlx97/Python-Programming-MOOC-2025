741a53ae60cf5d57bd11e6e90dfc056239ed1828
asdlx97 <delalieux.arno@gmail.com>
2025-11-16 19:19:16 +0100

    Add solved exercise implementing longest string finder function
    Added the longest(strings: list) function to return the longest string from a list. Included example usage and comments for clarity.

--- Exercises/tmcdata/mooc-programming-25/part05-01_longest_string/src/longest_string.py
+++ Exercises/tmcdata/mooc-programming-25/part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,41 @@
+""Please write a function named longest(strings: list), which takes a list of strings as its argument. The function finds and returns the longest string in the list. You may assume there is always a single longest string in the list.
+
+An example of expected behaviour:
+
+
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings))
+Sample output
+howdydoody""
+
 # Write your solution here
+def longest(strings: list):
+    longest_string = ""
+
+    for string in strings:
+        if len(string) > len(longest_string):
+            longest_string = string
+
+    return longest_string
+
+
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings))
+
+"""
+# Suggested Solution
+
+def longest(strings: list):
+    """ Function returns most longest of the strings """
+    longest = ""
+    for item in strings:
+        if len(item) > len(longest):
+            longest = item
+ 
+    return longest
+
+#Review
+same output, same script.
+"""
