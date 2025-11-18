26735690875b8d43fd3874f81ab834b04e9f9df2
asdlx97 <delalieux.arno@gmail.com>
2025-11-19 00:04:31 +0100

    Add solved exercise implementing create_tuple function
    Added the create_tuple function which takes three integers and returns a tuple containing the smallest, largest, and the sum of the arguments. Includes example usage and a suggested solution for reference.

--- Exercises/tmcdata/mooc-programming-25/part05-23_create_tuple/src/create_tuple.py
+++ Exercises/tmcdata/mooc-programming-25/part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,46 @@
+"""
+Please write a function named create_tuple(x: int, y: int, z: int),
+which takes three integers as its arguments, and creates and returns
+a tuple based on the following criteria:
+
+The first element in the tuple is the smallest of the arguments
+The second element in the tuple is the greatest of the arguments
+The third element in the tuple is the sum of the arguments
+An example of its use:
+
+
+if __name__ == "__main__":
+    print(create_tuple(5, 3, -1))
+
+Sample output
+(-1, 5, 7)
+"""
+
+
 # Write your solution here
+def create_tuple(x: int, y: int, z: int):
+    arguments = (x, y, z)
+    # arguments[x, y, z] #This also works
+
+    final_tuple = (min(arguments), max(arguments), sum(arguments))
+
+    return final_tuple
+
+
+if __name__ == "__main__":
+    print(create_tuple(5, 3, -1))
+
+"""
+# Suggested solution
+
+def create_tuple(x: int, y: int, z: int):
+    """ The function returns a tuple formed from the parameters (smallest, greatest, sum) """
+    smallest = min([x, y, z])
+    greatest = max([x, y, z])
+    sum = x + y + z # sum([x, y, z]) also works
+ 
+    return (smallest, greatest, sum)
+    
+#Review
+My solution gives the same correct output and is a bit more concise.
+"""
