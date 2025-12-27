68b0557f8523ff21d27c275846edebe1bb9f8987
asdlx97 <delalieux.arno@gmail.com>
2025-12-27 23:30:21 +0100

    Add solved exercise implementing separate_characters function
    Added the separate_characters function to split a string into ASCII letters, punctuation, and other characters using the string module. Includes example usage and explanatory comments.

--- Exercises/tmcdata/mooc-programming-25/part07-02_special_characters/src/special_characters.py
+++ Exercises/tmcdata/mooc-programming-25/part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,82 @@
+"""
+The Python module string contains some string constants,
+ which define certain groups of characters. These
+ include for example lowercase letters and punctuation
+ characters. Please familiarize yourself with these
+ constants, and then write a function named
+ separate_characters(my_string: str). The function
+ takes a string as its argument, and it should
+ separate the characters in the string into three
+ other strings, and return these in a tuple:
+
+The first string should contain the lowercase and
+uppercase ASCII letters (string constant ascii_letters)
+
+The second string should contain all punctuation
+characters defined by the string constant punctuation
+
+The third string should contain all the other characters
+(including whitespace)
+
+The characters should appear in the three strings in the same order as they appeared in the original string.
+
+An example of the function in action:
+
+parts = separate_characters("Olé!!! Hey, are ümläüts wörking?")
+print(parts[0])
+print(parts[1])
+print(parts[2])
+
+Sample output
+OlHeyaremltswrking
+!!!,?
+é  üäü ö
+"""
+
 # Write your solution here
+import string
+
+
+def separate_characters(my_string: str):
+    first = ""
+    second = ""
+    third = ""
+
+    for char in my_string:
+        if char in string.ascii_letters:
+            first += char
+        elif char in string.punctuation:
+            second += char
+        else:
+            third += char
+
+    return (first, second, third)
+
+
+if __name__ == "__main__":
+    print(separate_characters("Halloo??!!!! Ik ben Arnàà"))
+
+"""
+# Suggested solution
+
+ 
+from string import ascii_letters, punctuation
+ 
+def separate_characters(my_string: str):
+    letters = ""
+    special_characters = ""
+    other_characters = ""
+ 
+    for character in my_string:
+        if character in ascii_letters:
+            letters += character
+        elif character in punctuation:
+            special_characters += character
+        else:
+            other_characters += character
+ 
+    return (letters, special_characters, other_characters)
+ 
+#Review
+Same output, same script.
+"""
