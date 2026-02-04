f9a51dbc915683422e27243ade435a58d0e5f46f
asdlx97 <delalieux.arno@gmail.com>
2026-02-04 15:08:22 +0100

    Add solved exercise implementing Checklist, Customer and Cable classes
    Add exercise docstring and implement three simple data classes (Checklist, Customer, Cable) with constructors initializing attributes. The file also includes a duplicated 'Suggested solution' block and a brief review comment.

--- Exercises/tmcdata/mooc-programming-25/part08-06_three_classes/src/three_classes.py
+++ Exercises/tmcdata/mooc-programming-25/part08-06_three_classes/src/three_classes.py
@@ -1,1 +1,64 @@
+"""
+Please write a class named Book with the attributes name, author, genre and year, along with a constructor which assigns initial values to these attributes.
+
+Your class should work like this:
+
+python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+
+print(f"{python.author}: {python.name} ({python.year})")
+print(f"The genre of the book {everest.name} is {everest.genre}")
+
+Sample output
+Luciano Ramalho: Fluent Python (2015)
+The genre of the book High Adventure is autobiography
+
+"""
+
+
 # Write your solution here
+class Checklist:
+    def __init__(self, header: str, entries: list):
+        self.header = header
+        self.entries = entries
+
+
+class Customer:
+    def __init__(self, id: str, balance: float, discount: int):
+        self.id = id
+        self.balance = balance
+        self.discount = discount
+
+
+class Cable:
+    def __init__(self, model: str, length: float, max_speed: int, bidirectional: bool):
+        self.model = model
+        self.length = length
+        self.max_speed = max_speed
+        self.bidirectional = bidirectional
+
+
+"""
+# Suggested solution
+
+class Checklist:
+    def __init__(self, header: str, entries: list):
+        self.header = header
+        self.entries = entries
+ 
+class Customer:
+    def __init__(self, id: str, balance: float, discount: int):
+        self.id = id
+        self.balance = balance
+        self.discount = discount
+ 
+class Cable:
+    def __init__(self, model: str, length: float, max_speed: int, bidirectional: bool):
+        self.model = model
+        self.length = length
+        self.max_speed = max_speed
+        self.bidirectional = bidirectional
+
+#Review
+My solution results in the same output, is same script too.
+"""
