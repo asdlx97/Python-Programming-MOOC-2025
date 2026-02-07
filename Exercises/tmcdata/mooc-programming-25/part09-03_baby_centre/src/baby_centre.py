c549cef27967c26c11575a4811b8d0fda074a86f
asdlx97 <delalieux.arno@gmail.com>
2026-02-07 23:25:53 +0100

    Add solved exercise implementing BabyCentre methods and add examples
    Complete the BabyCentre exercise by implementing weigh, feed and weigh_ins. weigh now returns person.weight and increments an internal number_of_weigh_ins counter, feed increments the person's weight by one, and weigh_ins returns the total number of weigh-ins. Added a top-level docstring with exercise instructions and example usage/demo code as well as a suggested-solution comment. Also introduced the number_of_weigh_ins attribute to track weigh-ins.

--- Exercises/tmcdata/mooc-programming-25/part09-03_baby_centre/src/baby_centre.py
+++ Exercises/tmcdata/mooc-programming-25/part09-03_baby_centre/src/baby_centre.py
@@ -1,6 +1,136 @@
+"""
+NB: Some exercises have multiple parts, and you can receive points for the different parts separately. You can submit a partially completed exercise by choosing 'Submit Solution' from the menu next to the button for executing tests .
+
+The exercise template contains a class named Person and a skeleton implementation for the class BabyCentre. A BabyCentre object performs various actions on a Person object. It may, for example, weigh or feed the person. In this exercise you will implement the rest of the BabyCentre class. Please do not change the class definition of Person in any way.
+
+Weighing persons
+
+The BabyCentre class definition contains an outline for the function weigh:
+
+class BabyCentre:
+    def weigh(self, person: Person):
+        # return the weight of the person passed as an argument
+        return -1
+The method takes a Person object as its argument. It should return the weight of the person. You can access the weight of a person through the appropriate attribute defined in the Person class. Please fill in the rest of the implementation for the method weigh.
+
+Below is an example of a main function where a BabyCentre weighs two separate Person objects:
+
+baby_centre = BabyCentre()
+
+eric = Person("Eric", 1, 110, 7)
+peter = Person("Peter", 33, 176, 85)
+
+print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
+print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
+
+Sample output
+Eric weighs 7 kg
+Peter weighs 85 kg
+Feeding
+
+It is possible to change the state of an object passed as an argument. Please implement the method feed(person: Person) which increases by one the weight of the person passed as an argument.
+
+In the following example two persons are weighed, and then one of them is fed three times. Then the persons are weighed again:
+
+baby_centre = BabyCentre()
+
+eric = Person("Eric", 1, 110, 7)
+peter = Person("Peter", 33, 176, 85)
+
+print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
+print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
+print()
+
+baby_centre.feed(eric)
+baby_centre.feed(eric)
+baby_centre.feed(eric)
+
+print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
+print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
+The printout should reveal that Eric's weight has increased by three:
+
+Sample output
+Eric weighs 7 kg
+Peter weighs 85 kg
+
+Eric weighs 10 kg
+Peter weighs 85 kg
+Counting weigh-ins
+
+Please implement the method weigh_ins() which returns the total number of weigh-ins a BabyCentre object has performed. NB: you will need a new attribute for keeping track of the number of weigh-ins. You can use the following code to test your method:
+
+baby_centre = BabyCentre()
+
+eric = Person("Eric", 1, 110, 7)
+peter = Person("Peter", 33, 176, 85)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+Sample output
+Total number of weigh-ins is 0
+Total number of weigh-ins is 2
+Total number of weigh-ins is 6
+"""
+
 
 # WRITE YOUR SOLUTION HERE:
-#Note! Do not change the class Person!
+# Note! Do not change the class Person!
+class Person:
+    def __init__(self, name: str, age: int, height: int, weight: int):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+
+class BabyCentre:
+    def __init__(self):
+        self.number_of_weigh_ins = 0
+
+    def weigh(self, person: Person):
+        # return the weight of the person passed as an argument
+        self.number_of_weigh_ins += 1
+        return person.weight
+
+    def feed(self, person: Person):
+        person.weight += 1
+
+    def weigh_ins(self):
+        return self.number_of_weigh_ins
+
+
+if __name__ == "__main__":
+    baby_centre = BabyCentre()
+
+    eric = Person("Eric", 1, 110, 7)
+    peter = Person("Peter", 33, 176, 85)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
 
 class Person:
     def __init__(self, name: str, age: int, height: int, weight: int):
@@ -9,10 +139,27 @@ class Person:
         self.height = height
         self.weight = weight
 
+
+"""
+# Suggested solution
+
 class BabyCentre:
     def __init__(self):
         self.number_of_weigh_ins = 0
 
     def weigh(self, person: Person):
         # return the weight of the person passed as an argument
-        return -1
+        self.number_of_weigh_ins += 1
+        return person.weight
+
+    def feed(self, person: Person):
+        person.weight += 1
+
+    def weigh_ins(self):
+        return self.number_of_weigh_ins
+
+
+#Review
+My solution results in the same output, suggested 
+script is the same.
+"""
