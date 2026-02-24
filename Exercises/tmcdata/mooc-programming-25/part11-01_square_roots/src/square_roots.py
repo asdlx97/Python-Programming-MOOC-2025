4ece248c7ce36cae94af3437f4eb555708fbec2b
asdlx97 <delalieux.arno@gmail.com>
2026-02-24 20:51:26 +0100

    Add solved exercise implementing list comprehension returning square roots

--- Exercises/tmcdata/mooc-programming-25/part11-01_square_roots/src/square_roots.py
+++ Exercises/tmcdata/mooc-programming-25/part11-01_square_roots/src/square_roots.py
@@ -1,1 +1,46 @@
+"""
+Please write a function named square_roots(numbers: list) which takes a list of integers as its argument. The function should return a new list containing the square roots of the original integers.
+
+The math module from the Python standard library contains a suitable function for calculating the square root.
+
+The function should use a list comprehension. The maximum length of the function is two lines of code, including the header line beginning with the def keyword.
+
+The function should work as follows:
+
+lines = square_roots([1,2,3,4])
+for line in lines:
+    print(line)
+
+Sample output
+1.0
+1.4142135623730951
+1.7320508075688772
+2.0
+"""
+
+from math import sqrt
+
+
 # WRITE YOUR SOLUTION HERE:
+def square_roots(numbers: list):
+    return [sqrt(x) for x in numbers]
+
+
+if __name__ == "__main__":
+    lines = square_roots([1, 2, 3, 4])
+    for line in lines:
+        print(line)
+
+"""
+#Suggested solution
+
+from math import sqrt
+ 
+def square_roots(numbers: list):
+    return [sqrt(number) for number in numbers]
+ 
+
+#Review
+My solution results in the same output, the suggested one is the same.
+
+"""
