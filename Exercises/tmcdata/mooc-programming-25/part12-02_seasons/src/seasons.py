5b8b97015c8a1d780723b9b0e09f03f321159b22
asdlx97 <delalieux.arno@gmail.com>
2026-02-26 08:01:33 +0100

    Add solved exercise implementing nested sorting function by amount of seasons

--- Exercises/tmcdata/mooc-programming-25/part12-02_seasons/src/seasons.py
+++ Exercises/tmcdata/mooc-programming-25/part12-02_seasons/src/seasons.py
@@ -1,1 +1,41 @@
+"""
+Please write a function named sort_by_seasons(items: list) which takes a list of dictionaries as its argument. Each dictionary contains the information of a single TV show. The function should sort this list by the number of seasons each show has, in ascending order. The function should not change the original list, but return a new list instead.
+
+The function should work as follows:
+
+shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+for show in sort_by_seasons(shows):
+    print(f"{show['name']} {show['seasons']} seasons")
+
+Sample output
+
+Dexter 9 seasons
+Friends 10 seasons
+Simpsons 32 seasons
+"""
 # Write your solution here:
+def sort_by_seasons(items: list):
+    def show_by_seasons(item: dict):
+        return item["seasons"]
+
+    return sorted(items, key=show_by_seasons)
+
+if __name__ == "__main__":
+    shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+    for show in sort_by_seasons(shows):
+        print(f"{show['name']} {show['seasons']} seasons")
+
+"""
+#Suggested solution
+
+def sort_by_seasons(items: list):
+    def season_order(item):
+        return item["seasons"]
+        
+    return sorted(items, key=season_order)
+
+#Review
+My solution results in the same output, the suggested one is the same script.
+"""
