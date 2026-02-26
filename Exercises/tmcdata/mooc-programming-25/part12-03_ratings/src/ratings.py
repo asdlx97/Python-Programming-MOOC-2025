d6af8284ce194e57f2340f2572196f2718e941bf
asdlx97 <delalieux.arno@gmail.com>
2026-02-26 09:05:08 +0100

    Add solved exercise implementing nested sorting function by ratings

--- Exercises/tmcdata/mooc-programming-25/part12-03_ratings/src/ratings.py
+++ Exercises/tmcdata/mooc-programming-25/part12-03_ratings/src/ratings.py
@@ -1,1 +1,43 @@
+"""
+Please write a function named sort_by_ratings(items: list) which takes a list of dictionaries as its argument. The structure of the dictionaries is identical to the previous exercise. This function should sort the dictionaries in descending order based on the shows' ratings. The function should not change the original list, but return a new list instead.
+
+shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+print("Rating according to IMDB")
+for show in sort_by_ratings(shows):
+    print(f"{show['name']}  {show['rating']}")
+
+Sample output
+Rating according to IMDB
+Friends 8.9
+Simpsons 8.7
+Dexter 8.6
+"""
+
+
 # TEE RATKAISUSI TÄHÄN:
+def sort_by_ratings(items: list):
+    def show_by_ratings(item: dict):
+        return item["rating"]
+
+    return sorted(items, key=show_by_ratings, reverse=True)
+
+if __name__ == "__main__":
+    shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+    print("Rating according to IMDB")
+    for show in sort_by_ratings(shows):
+        print(f"{show['name']}  {show['rating']}")
+
+"""
+#Suggested solution
+
+def sort_by_seasons(items: list):
+    def season_order(item):
+        return item["seasons"]
+        
+    return sorted(items, key=season_order)
+
+#Review
+My solution results in the same output, the suggested one is the same script.
+"""
