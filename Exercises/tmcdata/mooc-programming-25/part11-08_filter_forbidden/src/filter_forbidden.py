b4c239f7cd1d980c80820d4daec744e68563ddb5
asdlx97 <delalieux.arno@gmail.com>
2026-03-14 08:30:06 +0100

    Add solved exercise implementing list comprehension using join() method

--- Exercises/tmcdata/mooc-programming-25/part11-08_filter_forbidden/src/filter_forbidden.py
+++ Exercises/tmcdata/mooc-programming-25/part11-08_filter_forbidden/src/filter_forbidden.py
@@ -1,1 +1,36 @@
+"""
+Please write a function named filter_forbidden(string: str, forbidden: str) which takes two strings as its arguments. The function should return a new version of the first string. It should not contain any characters from the second string.
+
+The function should be implemented using list comprehensions. The maximum length of the function is three lines of code, including the header line beginning with the def keyword.
+
+Please have a look at the example below.
+
+sentence = "Once! upon, a time: there was a python!??!?!"
+filtered = filter_forbidden(sentence, "!?:,.")
+print(filtered)
+
+Sample output
+Once upon a time there was a python
+"""
+
 # WRITE YOUR SOLUTION HERE:
+def filter_forbidden(string: str, forbidden: str):
+    return "".join([letter for letter in string if letter not in forbidden])
+
+
+if __name__ == "__main__":
+    sentence = "Once! upon, a time: there was a python!??!?!"
+    filtered = filter_forbidden(sentence, "!?:,.")
+    print(filtered)
+
+"""
+#Suggested solution
+
+def filter_forbidden(string: str, forbidden: str):
+    return "".join([character for character in string if character not in forbidden])
+ 
+
+#Review
+My solution results in the same output, the suggested one is the same script.
+
+"""
