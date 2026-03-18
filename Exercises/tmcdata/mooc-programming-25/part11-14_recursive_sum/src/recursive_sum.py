24aaff7b83deeb95c34199af9c1380e6c6460ec3
asdlx97 <delalieux.arno@gmail.com>
2026-03-18 11:26:04 +0100

    Add solved exercise implementing recursive sum

--- Exercises/tmcdata/mooc-programming-25/part11-14_recursive_sum/src/recursive_sum.py
+++ Exercises/tmcdata/mooc-programming-25/part11-14_recursive_sum/src/recursive_sum.py
@@ -1,1 +1,31 @@
 # WRITE YOUR SOLUTION HERE:
+def recursive_sum(number: int):
+    # if the number is 1, there is nothing else to add
+    if number <= 1:
+        return number
+
+    # fill in the rest of the function
+    return number + recursive_sum(number-1)
+
+if __name__ == "__main__":
+    result = recursive_sum(3)
+    print(result)
+
+    print(recursive_sum(5))
+    print(recursive_sum(10))
+
+"""
+#Suggested solution
+
+def recursive_sum(number: int):
+    # when the number is 1, there are no other summable...
+    if number <= 1:
+        return number
+ 
+    return number + recursive_sum(number - 1)
+    
+# WRITE YOUR SOLUTION HERE:
+ 
+#Review
+My solution results in the same output, the suggested one is exactly the same.
+"""
